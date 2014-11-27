7fdae164557ebd8c76596ee76c416d5c5b85b6eb
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2014-11-27 05:40:49 +0100

    Adding Gruntfile and gulp file exmaples

---
+++ INDUS/Gruntfile.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = function(grunt) {
+  
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '',
+    // Task configuration.
+    clean: {
+      src: ['app/dist']
+    },
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['app/js/**/*.js' ],
+        dest: 'app/dist/grunt.<%= pkg.name %>.js'
+      },
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'app/dist/grunt.<%= pkg.name %>.min.js'
+      },
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task.
+  grunt.registerTask('default', ['clean', 'concat', 'uglify']);
+
+};
